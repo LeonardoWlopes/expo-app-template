8763c3c2b6857e04bbd8b2cc236c89eba01f3bc4
Leonardo Lopes <leonardo@buildbox.com.br>
2024-12-17 10:32:54 -0300

    chore: add some utils

---
+++ src/utils/env.ts
@@ -0,0 +1,26 @@
+import { t } from '@/i18n/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+	BASE_URL: z.string().url(),
+})
+
+const _env = envSchema.safeParse({
+	BASE_URL: process.env.EXPO_PUBLIC_BASE_URL,
+})
+
+if (!_env.success) {
+	const errorTable = Object.entries(_env.error.flatten().fieldErrors).reduce(
+		(acc, [key, value]) => {
+			acc[key] = value.join(', ')
+			return acc
+		},
+		{} as Record<string, string>,
+	)
+
+	throw new Error(
+		`${t('ERROS.ENVIRONMENT_VARIABLES')} \n${JSON.stringify(errorTable, null, 2)}`,
+	)
+}
+
+export const env = _env.data
